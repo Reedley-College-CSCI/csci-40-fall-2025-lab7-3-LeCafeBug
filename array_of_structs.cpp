5a4409fa7ca6e96341824645dbf80c914e68ae73
Edgar Jarquin <edgarjarquin3@gmail.com>
2025-10-13 14:31:08 -0700

    Implemented readTemperatures function to load data into array of structs

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -16,13 +16,13 @@ struct TemperatureRecord
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(??); // TODO: Fix the parameters
+void readTemperatures(int day, int temperature); // TODO: Fix the parameters
 void printTemperatures(const ???);
 TemperatureRecord findMin(const ???);
 TemperatureRecord findMax(const ???);
 double findAverage(const ???);
 
-TemperatureRecord [MAX_DAYS];
+TemperatureRecord DayAndTemp [MAX_DAYS];
 
 int main() {
     // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
@@ -30,9 +30,13 @@ int main() {
     int size = 0;  // Actual number of records read
 
     // TODO: Step 3 - Call readTemperatures() to load data from file
+    readTemperatures(day, temperature);
 
     // TODO: Step 4 - Print the temperatures
+    for (int i = 0; i < SIZE) {
+        cout << << endl;
 
+    }
     // TODO: Step 5 - Compute and display min, max, and average temperature
 
     return 0;
@@ -40,6 +44,19 @@ int main() {
 
 // TODO: Step 6 - Implement readTemperatures()
 // Read from "temps.txt" and store data in the array
+void readTemperatures(int day, int temperature)
+{
+    ifstream file;
+    file.open("temps.txt");
+    if (!file) {
+        cout << "File open failure!";
+    }
+
+    while(count < MAX_DAYS && (file >> TemperatureRecord[count].day)) {
+        file >> TemperatureRecord[count].temperature;
+        count++ 
+    }
+}
 
 // TODO: Step 7 - Implement printTemperatures()
 // Print all stored temperatures in a formatted table
